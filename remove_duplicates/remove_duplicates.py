a77058a060709ddd81f982771e5d667a72ab0a55
jumbobumbo <dwheels91@gmail.com>
2020-03-01 17:19:02 +0000

    remove duplicates using dict.fromkeys - straight into master!!!!!

--- remove_duplicates/remove_duplicates.py
+++ remove_duplicates/remove_duplicates.py
@@ -11,3 +11,12 @@ def remove_duplicate_elem_from_lst(list1):
         if elem not in wanted_elements:
             wanted_elements.append(elem)
     return wanted_elements
+
+
+def remove_duplicate_elems_from_lst_with_dict(list1):
+    """
+    utilises dict.fromkeys to be a lil cheeky (dict keys cannot have duplicates - does the job for us :O)
+    :param list1: list
+    :return: list
+    """
+    return list(dict.fromkeys(list1))
